efdc79ddf86b4c8ce9baa5a64440385fdae973d8
LosUnidades <1032540385@qq.com>
2019-07-21 15:46:17 +0800

    init repo

---
+++ src/main/java/life/dan/life/LifeApplication.java
@@ -0,0 +1,13 @@
+package life.dan.life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeApplication.class, args);
+    }
+
+}
